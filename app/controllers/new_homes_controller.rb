35b98528ae5f27c2fc57559aa8eaf50d60c3927d
Hc <82017876@qq.com>
2014-06-04 15:53:31 +0800

    new hous list alter

--- app/controllers/new_homes_controller.rb
+++ app/controllers/new_homes_controller.rb
@@ -7,7 +7,25 @@ class NewHomesController < ApplicationController
                         .paginate(page: params[:page])
                         .per_page(5)
 
+    @dummy_data = DummyData.where("today_hit>=1").order("created_at desc")
+    @list = []
+    @dummy_data.each do |dummy|
+      @list << [dummy.new_home_id]
+     end 
+
+    @want = DummyData.where("want>=1 ").order("created_at asc")
+    @want_id = []
+    @want.each do |want|
+      @want_id << [want.new_home_id]
+     end
+
     @keyword = Sunspot.search(NewHome) do
+      per_page = params[:per_page]
+      if per_page.blank?
+        per_page = 10
+      else
+        per_page = params[:per_page]
+      end
       keywords params[:keyword]
       with(:area_id).equal_to(params[:area_id]) if params[:area_id].present?
       with(:status_id).equal_to(params[:status_id]) if params[:status_id].present?
@@ -16,7 +34,7 @@ class NewHomesController < ApplicationController
       with(:property_type_id).equal_to(params[:property_type_id]) if params[:property_type_id].present?
       with(:price).between(params[:mix]..params[:max]) if params[:mix].present? && params[:max].present?
       order_by :created_at, :desc
-      paginate :page => params[:page], :per_page => 5
+      paginate :page => params[:page], :per_page => per_page
     end
 
     @new_homes = @keyword.results
@@ -40,19 +58,19 @@ class NewHomesController < ApplicationController
                        .per_page(15)
     if IntentionToRegister.where(new_home_id: params[:id]).blank?
       (rand(9) + 1).times do
-        IntentionToRegister.create!(new_home_id: params[:id]) 
+        IntentionToRegister.create!(new_home_id: params[:id])
       end
     end
 
-    DummyData.create!(new_home_id: params[:id],today_hit: 1) 
+    DummyData.create!(new_home_id: params[:id],today_hit: 1)
     (rand(49) + 1).times do
-      DummyData.create!(new_home_id: params[:id],today_hit: 2) 
+      DummyData.create!(new_home_id: params[:id],today_hit: 2)
     end
     (rand(2) + 1).times do
-      DummyData.create!(new_home_id: params[:id],read: 2) 
+      DummyData.create!(new_home_id: params[:id],read: 2)
     end
     (rand(1) + 1).times do
-      DummyData.create!(new_home_id: params[:id],want: 2) 
+      DummyData.create!(new_home_id: params[:id],want: 2)
     end
 
     @today_hit = DummyData.where(new_home_id: params[:id],today_hit:1..2,created_at: Time.now.beginning_of_day..Time.now.end_of_day).count
